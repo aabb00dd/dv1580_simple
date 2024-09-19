36d5949938beaf4ba04b1737eb32a5990898a697
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:19:20 +0200

    Update add.c

--- add.c
+++ add.c
@@ -3,13 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-    
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    
+    // Check if the number of arguments is correct
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <num1> <num2>\n", argv[0]);
+        return 1;
+    }
+
+    // Convert arguments to integers
+    char *endptr;
+    int num1 = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0') {
+        fprintf(stderr, "Invalid integer: %s\n", argv[1]);
+        return 1;
+    }
+    int num2 = strtol(argv[2], &endptr, 10);
+    if (*endptr != '\0') {
+        fprintf(stderr, "Invalid integer: %s\n", argv[2]);
+        return 1;
+    }
+
+    // Calculate the sum
     int sum = num1 + num2;
 
+    // Print the result
     printf("%d\n", sum);
 
-  return 0;
+    return 0;
 }
