6e07de35b0cf8871e107e5e21f919a94673c4406
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:28:58 +0200

    Update div.c

--- div.c
+++ div.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    if (argc != 3) 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
 
-  return 0;
+    char *endptr;
+    int num1 = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+    
+    int num2 = strtol(argv[2], &endptr, 10);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+
+    int sum = num1 / num2;
+    printf("%d\n", sum);
+
+    return 0;
 }
