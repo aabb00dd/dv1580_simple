740d2d520c2e99ab61a501d77d1abadce8225371
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:28:40 +0200

    Update add.c

--- add.c
+++ add.c
@@ -3,29 +3,28 @@
 
 int main(int argc, char *argv[])
 {
-    // Check if the number of arguments is correct
-    if (argc != 3) {
+    if (argc != 3) 
+    {
         printf("-ERROR-\n");
         return 1;
     }
 
-    // Convert arguments to integers
     char *endptr;
     int num1 = strtol(argv[1], &endptr, 10);
-    if (*endptr != '\0') {
+    if (*endptr != '\0') 
+    {
         printf("-ERROR-\n");
         return 1;
     }
+    
     int num2 = strtol(argv[2], &endptr, 10);
-    if (*endptr != '\0') {
+    if (*endptr != '\0') 
+    {
         printf("-ERROR-\n");
         return 1;
     }
 
-    // Calculate the sum
     int sum = num1 + num2;
-
-    // Print the result
     printf("%d\n", sum);
 
     return 0;
