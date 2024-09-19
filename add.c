807459f955b42684c269a75552d946e465709d0f
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:17:34 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_number(const char *str) {
-    while (*str) 
-    {
-        if (!isdigit(*str)) return 0;
-        str++;
-    }
-    
-    return 1;
-}
-
 int main(int argc, char *argv[])
 {
-    if (!is_number(argv[1]) || !is_number(argv[2])) 
-    {
-    printf("-ERROR-");
-    return 1;
-    }
     
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
