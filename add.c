5c6b44c261c9ceabfc0f6aaf8b74934e2be574b5
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:06:12 +0200

    Update add.c

--- add.c
+++ add.c
@@ -3,9 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+    
+    if(!isdigit(argv[1]) || !isdigit(argv[2]))
+    {
+        Print("ERROR")
+        break    
+    }
+    
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
-
+    
     int sum = num1 + num2;
 
     printf("%d\n", sum);
