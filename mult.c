dc2a3adaefed847518144cc967df74a69494db6e
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:37:20 +0200

    Update mult.c

--- mult.c
+++ mult.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    if (argc != 3) 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
 
-  return 0;
+    char *endptr;
+    double num1 = strtod(argv[1], &endptr);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+
+    double num2 = strtod(argv[2], &endptr);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+
+    double result = num1 * num2;
+    printf("%f\n", result);
+
+    return 0;
 }
