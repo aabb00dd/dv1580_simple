624d82732abb5e6d3d793a172b67045a33c66d5c
Abod <112323349+aabb00dd@users.noreply.github.com>
2024-09-19 03:14:21 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_number(const char *str) {
+    while (*str) 
+    {
+        if (!isdigit(*str)) return 0;
+        str++;
+    }
+    
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
+    if (!is_number(argv[1]) || !is_number(argv[2])) 
+    {
+    fprintf(stderr, "Error: Both arguments must be valid numbers.\n");
+    return 1;
+    }
+    
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
     
